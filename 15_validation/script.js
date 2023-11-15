0cc6c8a3e65a42d70c6b2dda3fb9aba32e5215ac
MoeMinThway <moeminthway.mink@gmail.com>
2023-11-16 00:25:08 +0630

    Second time

--- 15_validation/script.js
+++ 15_validation/script.js
@@ -37,3 +37,41 @@ function validatePhone(){
     return true;
 
 }
+function validateEmail(){
+    var email = document.getElementById("contact-email").value;
+
+    if(email.length == 0){
+        emailError.innerHTML = 'Email is required';
+        return false;
+    }
+    if(!email.match(/^[A-Za-z\._\-[0-9]*[@][A-Za-z]*[\.][a-z]{2,4}$/)){
+        emailError.innerHTML= 'Email Invalid';
+        return false;
+    }
+    emailError.innerHTML = '<i class="fa-solid fa-circle-check"></i>'
+    return true;
+}
+function validateMessage(){
+    var message = document.getElementById("contact-message").value;
+    var required = 30;
+    var left  = required - message.length;
+
+    if( left > 0){
+        messageError.innerHTML= left + ' more characters required' ;
+        return false;
+    }
+
+    messageError.innerHTML = '<i class="fa-solid fa-circle-check"></i>'
+    return true;
+}
+function validateForm(){
+    if(!validateName () || !validatePhone() || !validateEmail() || !validateMessage()){
+        submitError.style.display ="block";
+        submitError.innerHTML="Please fix error to submit";
+        setInterval(function(){
+            submitError.style.display ="none";
+
+        },3000);
+        return false;
+    }
+}
